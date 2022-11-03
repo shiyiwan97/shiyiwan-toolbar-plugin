6cd9d497fabad38af9f9b97cbaf99dbfb4e16737
shiyiwan97 <1057862180@qq.com>
2022-11-04 02:08:10 +0800

    date: 2022-10-29 submitter: shiyiwan desc: excel compare util

---
+++ src/main/java/com/shiyiwan/plugin/ToolbarFactory.java
@@ -0,0 +1,28 @@
+package com.shiyiwan.plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class ToolbarFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+        ToolbarPanel toolbarPanel = new ToolbarPanel();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        JBScrollPane jbScrollPane = new JBScrollPane(toolbarPanel.getPanel());
+        jbScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        Content content = contentFactory.createContent(jbScrollPane, "ExcelUtils", false);
+        content.setPreferredFocusableComponent(jbScrollPane);
+        toolWindow.getContentManager().addContent(content);
+
+    }
+
+}
